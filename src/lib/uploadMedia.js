d17766d1226f704448ac593ef1596ac5b3898b37
Tikka-byte <theekshanajpk640@gmail.com>
2026-07-30 10:52:07 +0530

    test update✏️

---
+++ src/lib/uploadMedia.js
@@ -0,0 +1,32 @@
+import {createClient} from "@supabase/supabase-js";
+
+const key ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzZXFjdWhvYmpta3hqaGZyemxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyNzA2MTcsImV4cCI6MjEwMDg0NjYxN30.A9l76o9C6Ca_Y9RV3KR-qac92o4Id_OY-t3-VQ1zA7c";
+const url = "https://hseqcuhobjmkxjhfrzli.supabase.co";
+
+const supabase = createClient(url, key);
+
+export default function uploadMedia(file){
+
+    return new Promise(
+        (resolve, reject)=>{
+            if(file == null){
+                reject("File is null");
+            }else{
+
+                const timestamp = new Date().getTime();
+                const fileName = timestamp + "_" + file.name;
+
+
+                supabase.storage.from("images").upload(fileName,file).then(
+                    () => {
+                        const publicUrl = supabase.storage.from("images").getPublicUrl(fileName).data.publicUrl;
+                        resolve(publicUrl);
+                    }
+                ).catch((err) => {
+                    reject(err);
+                });
+            }
+        }
+    )
+
+}
--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,36 +1,95 @@
-import {useState} from "react";
+import { useState } from "react";
+import uploadMedia from "../lib/uploadMedia";
 import { toast } from "react-hot-toast";
 
+
+
 export default function TestPage() {
-    const [status, setStatus] = useState("on");
-    const [loading, setLoading] = useState(false);
 
+    const [file, setFiles] = useState(null);
+
+    function uploadFiles() {
+
+        uploadMedia(file).then(
+            (res)=>{
+                console.log(res);
+                toast.success("File uploaded successfully");
+            }
+        ).catch((err) => {
+            toast.error("Error uploading file");
+            console.error(err);
+        });
+
+
+         console.log(file);
+        // supabase.storage.from("images").upload(file.name,file).then(
+        //     () => {
+        //     const publicUrl = supabase.storage.from("images").getPublicUrl(file.name).data.publicUrl;
+        //     console.log(publicUrl);
+        // }).catch(() => {
+        //     toast.error("Error uploading file");
+        // });
+    }
     return (
-        <div className="w-full h-screen flex items-center justify-center flex-col " >
-            <h1 className="text-3xl font-bold text-center w-full" >{status}</h1>
-            <div className="w-75 h-[50px] flex justify-center items-center gap-2 " >
-                <button onClick={
-                    ()=>{
-                        setStatus("on");
-                        setLoading(true);
-                        toast.success("Turned On");
-                    }
-                } className="p-2 text-white bg-green-600">Turn On</button>
-                <button onClick={()=>{
-                        setStatus("off");
-                        setLoading(false);
-                        toast.success("Turned Off");
-                    }} className="p-2 text-white bg-red-600">Turn Off</button>
-                <button onClick={()=>{
-                        setStatus(status === "on" ? "off" : "on");
-                    }} className="p-2 text-white bg-blue-600">Toggle</button>
+        <div className="w-full h-screen flex items-center justify-center" >
+           
+               <input type="file" 
+               multiple={true}
+               onChange={(e) => 
+                    setFiles(e.target.files[0])
 
-                    
-            </div>
-            {loading && <h1 className="text-2xl font-bold">Loading...</h1> }
+               } />
+               <button onClick={uploadFiles} className="bg-blue-700 text-white p-2 rounded-lg ">Submit</button>
+          
+       
         </div>
     )
+
 }
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzZXFjdWhvYmpta3hqaGZyemxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyNzA2MTcsImV4cCI6MjEwMDg0NjYxN30.A9l76o9C6Ca_Y9RV3KR-qac92o4Id_OY-t3-VQ1zA7c
+//https://hseqcuhobjmkxjhfrzli.supabase.co
+
+//Tikka@1128#
+
+// import {useState} from "react";
+// import { toast } from "react-hot-toast";
+
+
+
+
+// export default function TestPage() {
+
+//     //hooks
+//     const [status, setStatus] = useState("on");
+//     const [loading, setLoading] = useState(false);
+
+//     return (
+//         <div className="w-full h-screen flex items-center justify-center flex-col " >
+//             <h1 className="text-3xl font-bold text-center w-full" >{status}</h1>
+//             <div className="w-75 h-[50px] flex justify-center items-center gap-2 " >
+//                 <button onClick={
+//                     ()=>{
+//                         setStatus("on");
+//                         setLoading(true);
+//                         toast.success("Turned On");
+//                     }
+//                 } className="p-2 text-white bg-green-600">Turn On</button>
+//                 <button onClick={()=>{
+//                         setStatus("off");
+//                         setLoading(false);
+//                         toast.success("Turned Off");
+//                     }} className="p-2 text-white bg-red-600">Turn Off</button>
+//                 <button onClick={()=>{
+//                         setStatus(status === "on" ? "off" : "on");
+//                     }} className="p-2 text-white bg-blue-600">Toggle</button>
+
+                    
+//             </div>
+//             {loading && <h1 className="text-2xl font-bold">Loading...</h1> }
+//         </div>
+//     )
+// }
 
 
 // export default function TestPage() {
